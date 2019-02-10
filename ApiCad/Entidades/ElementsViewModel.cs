6e99c33e9a90b6feb5d0793e47999cda33f2778b
rrochow <rcr2rs@gmail.com>
2019-02-09 22:14:33 -0300

    Primer commit

---
+++ ApiCad/Entidades/ElementsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCad.Entidades
+{
+    public class ElemntsViewModel
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Image { get; set; }
+    }
+}
